7e0e3015695ca8fa1c3242981d8d7b2024d9875e
qkhai15 <2251061804@e.tlu.edu.vn>
2025-08-06 23:23:53 +0700

    BTH5.2

---
+++ Bai5.2/app/src/main/java/com/example/bai52/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.bai52;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.text.DecimalFormat;
+
+public class MainActivity extends Activity {
+    Button btnTieptuc, btnGiai, btnThoat;
+    EditText edita, editb, editc;
+    TextView txtkq;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnTieptuc = findViewById(R.id.btntieptuc);
+        btnGiai = findViewById(R.id.btngiaipt);
+        btnThoat = findViewById(R.id.btnthoat);
+        edita = findViewById(R.id.edta);
+        editb = findViewById(R.id.edtb);
+        editc = findViewById(R.id.edtc);
+        txtkq = findViewById(R.id.txtkq);
+
+        btnGiai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sa = edita.getText().toString();
+                String sb = editb.getText().toString();
+                String sc = editc.getText().toString();
+                int a = Integer.parseInt(sa);
+                int b = Integer.parseInt(sb);
+                int c = Integer.parseInt(sc);
+                String kq = "";
+                DecimalFormat dcf = new DecimalFormat("0.00");
+
+                if (a == 0) {
+                    if (b == 0) {
+                        if (c == 0)
+                            kq = "PT vô số nghiệm";
+                        else
+                            kq = "PT vô nghiệm";
+                    } else {
+                        kq = "Pt có 1 No, x=" + dcf.format(-c / (double)b);
+                    }
+                } else {
+                    double delta = b * b - 4 * a * c;
+                    if (delta < 0) {
+                        kq = "PT vô nghiệm";
+                    } else if (delta == 0) {
+                        kq = "Pt có No kép x1=x2=" + dcf.format(-b / (2.0 * a));
+                    } else {
+                        kq = "Pt có 2 No: x1=" + dcf.format((-b + Math.sqrt(delta)) / (2.0 * a)) +
+                                "; x2=" + dcf.format((-b - Math.sqrt(delta)) / (2.0 * a));
+                    }
+                }
+                txtkq.setText(kq);
+            }
+        });
+
+        btnTieptuc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edita.setText("");
+                editb.setText("");
+                editc.setText("");
+                edita.requestFocus();
+            }
+        });
+
+        btnThoat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
